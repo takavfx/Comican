19d53c8d2283befefd7e7d3ccfa6967a4e6448ec
Takanori Kishikawa <auratus.lemma@gmail.com>
2020-03-06 02:34:12 +0900

    Trun display option for BookAdmin list.

--- mysite/comican/admin.py
+++ mysite/comican/admin.py
@@ -1,20 +1,39 @@
 from django.contrib import admin
 
 from .models import Circle, Auther, Book, Page, TagCategory, Tag, Copyright, Series, Character
-reg_models = [
-    Circle,
-    Auther,
-    Book,
-    Page,
-    TagCategory,
-    Tag,
-    Copyright,
-    Series,
-    Character
-]
 
-def register_models(reg_models):
-    for reg_model in reg_models:
-        admin.site.register(reg_model)
 
-register_models(reg_models)
+
+class BookAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'series_number', 'favorite')
+    list_filter = ['favorite']
+
+
+
+
+def register_models():
+    
+    reg_model_sets = [
+        [Circle],
+        [Auther],
+        [Book, BookAdmin],
+        [Page],
+        [TagCategory],
+        [Tag],
+        [Copyright],
+        [Series],
+        [Character]
+    ]
+
+    for reg_model_set in reg_model_sets:
+        if not len(reg_model_set) == 2:
+            admin.site.register(reg_model_set[0])
+        else:
+            admin.site.register(reg_model_set[0], reg_model_set[1])
+
+
+def main():
+    register_models()
+
+main()
