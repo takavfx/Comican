8d9b05ee3c8f93a7b5b2ebdb8e718d9ecc651564
Takanori Kishikawa <auratus.lemma@gmail.com>
2020-03-15 03:34:02 +0900

    Change books root url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('comican/', include('comican.urls')),
+    path('books/', include('comican.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
