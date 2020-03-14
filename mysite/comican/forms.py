10fc368aec85550c19b24188a3caddcd8e8feb1c
Takanori Kishikawa <auratus.lemma@gmail.com>
2020-03-15 05:41:20 +0900

    Dynamically create forms by models

--- mysite/comican/forms.py
+++ mysite/comican/forms.py
@@ -7,34 +7,71 @@ class FileFieldForm(forms.Form):
 
 
 class AddBookForm(forms.ModelForm):
-    name = forms.CharField(label='Name',
-                        max_length=300,
-                        required=True,
-                        widget=forms.TextInput(attrs={'class': 'form-control',
-                                                    'placeholder': 'Book Name'}))
-    series = forms.ModelChoiceField(
-            queryset=Series.objects.all(),
-            label="Series",
-            widget=forms.Select(
-                attrs={'class': 'form-control'}
-            )
-        )
-    series_number = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    image = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}),
-    )
-    authors = forms.ModelMultipleChoiceField(
-            queryset=Auther.objects.all(),
-            label="Authors",
-            widget=forms.SelectMultiple(
-                attrs={
-                    'multiple': True,
-                    'class': 'form-control'}
-                    )
-        )
-    detail = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control',
-                                                        'placeholder': "Detail of the book"}))
+    template_name = 'comican/uploader.html'
+    # name = forms.CharField(label='Name',
+    #                     max_length=300,
+    #                     required=True,
+    #                     widget=forms.TextInput(attrs={'class': 'form-control',
+    #                                                 'placeholder': 'Book Name'}))
+    # series = forms.ModelChoiceField(
+    #         queryset=Series.objects.all(),
+    #         label="Series",
+    #         widget=forms.Select(
+    #             attrs={'class': 'form-control'}
+    #         ),
+    #         required=False
+    #     )
+    # series_number = forms.IntegerField(
+    #         widget=forms.NumberInput(attrs={'class': 'form-control'}),
+    #     )
+    # image = forms.ImageField(
+    #     widget=forms.ClearableFileInput(attrs={'multiple': True}),
+    # )
+    # authors = forms.ModelMultipleChoiceField(
+    #         queryset=Auther.objects.all(),
+    #         label="Authors",
+    #         widget=forms.SelectMultiple(
+    #             attrs={
+    #                 'multiple': True,
+    #                 'class': 'form-control'}
+    #                 ),
+    #         required=False
+    #     )
+    # detail = forms.CharField(
+    #     required=False,
+    #     widget=forms.Textarea(
+    #         attrs={'class': 'form-control',
+    #             'placeholder': "Detail of the book"}))
+
 
     class Meta:
+        model = Book
+        fields = '__all__' 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+
+
+class AddPageForm(forms.ModelForm):
+    class Meta:
         model = Page
-        fields = ('name', 'image', 'series_number')
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+
+class AddAuthorForm(forms.ModelForm):
+    class Meta:
+        model = Auther
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
